86a78793359828888fb1e80292debcdce3ab00ae
Hwan <44517145+buddha1111@users.noreply.github.com>
2019-05-27 22:31:41 +0900

    setting commit

---
+++ BambooReview/src/common/listener/SessionCounterListener.java
@@ -0,0 +1,36 @@
+package common.listener;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@WebListener
+public class SessionCounterListener implements HttpSessionListener{
+
+	private static int activeSessions;
+	
+	
+	@Override
+	public void sessionCreated(HttpSessionEvent se) {
+		HttpSession session  = se.getSession();
+		System.out.println("[sid@SessionCounterListener.sessionCreated="+session.getId()+"]");
+		
+		//생성된 세션객체수 1증가
+		activeSessions++;
+		System.out.println("[현재 세션객체수: "+activeSessions+"]");
+		
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent se) {
+		HttpSession session = se.getSession();
+		System.out.println("[sid@SessionCounterListener.sesionDestroyed="+session.getId()+"]");
+		
+		activeSessions--;
+		System.out.println("[현재 세션객체수: "+activeSessions+"]");
+		
+	}
+	
+	
+}
