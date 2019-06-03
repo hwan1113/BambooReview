2ec7470127a94eea15c2d1701b640783859cf2e7
user1 <user1@user1-PC>
2019-06-03 15:22:14 +0900

    7일자 오후 3번째 수정

--- BambooReview/src/review/controller/ReviewLikeCntServlet.java
+++ BambooReview/src/review/controller/ReviewLikeCntServlet.java
@@ -7,39 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import review.model.service.ReviewService;
-
 /**
- * Servlet implementation class ReviewDeleteServlet
+ * Servlet implementation class ReviewLikeCntServlet
  */
-@WebServlet("/review/reviewDelete")
-public class ReviewDeleteServlet extends HttpServlet {
+@WebServlet("/review/reviewLikeCnt")
+public class ReviewLikeCntServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//1.파라미터 핸들링
-		int reviewNo = Integer.parseInt(request.getParameter("reviewNo"));
-		System.out.println("reviewNo@serv="+reviewNo);
-
-		//2. 업무로직
-		int result = new ReviewService().deleteReview(reviewNo);
+		//1. 파라미터 핸들링
 		
-		//3. view단 처리
-		String msg = "";
-		if(result>0) {
-			msg = "게시물 삭제 성공!";
-		}
-		else {
-			msg = "게시물 삭제 실패!";
-		}
-		
-		request.setAttribute("msg", msg);
-		request.setAttribute("loc", "/review/reviewList");
-		request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp")
-			   .forward(request, response);
 	}
 
 	/**
